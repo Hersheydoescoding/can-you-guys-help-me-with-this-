e2a555be03c892410e3034fd4b356a15c4751789
Hersheydoescoding <98904005+Hersheydoescoding@users.noreply.github.com>
2022-02-02 20:22:15 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+
+
+print ("Hi my name is Harsith")
+input ("What is your name?:")
+print ("wow that's a nice name")
+input ("do you wanna play a game?:")
+print ("very well then hop in")
+print ("****************************")
+print (" the hershey boi quiz")
+print ("*****************************")
+input ("are you ready to play the game?:")
+print ("alrighty lets play the game")
+
+answers - input ("question 1 where was harsith born Counrty and state")
+if answers == "India,Tamilnadu"
+ print("Thats correct!!")
+elif feeling == "india,tamilnadu"
+ print ("Thats correct!!")
+else:
+  print ("ohuh seems like you got the wrong answer better luck next time")
+
+ans - input ("question 2 which year was Harsith born?:")
+if ans == "2007"
+ print ("thats correct!!")
+else:
+  print ("ohuh seems like you got the wrong answer better luck next time")
+
+ans3 - input ("ok question 3 which is harsith's fav colour ")
+if ans3 == "Blue"
+ print ("Thats correct!!")
+else:
+  print ("ohuh seems like you got the wrong answer better luck next time")
+print ("alright folks")
+print ("Thats enough for today")
+input ("did you enjoy the game?:")
+print ("thanks for the reply")
+print ("see ya")
+
